a54ec27364d80cd76a0fe76c805da26b014a72f6
coder6497 <rty.sem@yandex.ru>
2025-09-01 18:16:57 +0300

    moved code files to root dir

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from database import create_db_and_tables
 app = FastAPI(docs_url='/docs', redoc_url='/redoc')
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://127.0.0.1:8001", "http://127.0.0.1:8000", "http://localhost:8000", 'https://vibecoder-hex-brainrotai-6cd6.twc1.net'],
+                   allow_origins=["http://127.0.0.1:8001", "http://127.0.0.1:8000", "http://localhost:8000", 'https://vibecoder-hex-brainrotai-6cd6.twc1.net', 'https://vibecoder-hex-brainrotaiclient-2b95.twc1.net'],
                    allow_credentials=True,
                     allow_methods=["*"],
                     allow_headers=["*"],
