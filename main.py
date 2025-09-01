0efa8251b3c8c2065dc8edefe018e416a1f124b9
coder6497 <rty.sem@yandex.ru>
2025-09-01 17:47:34 +0300

    moved code files to root dir

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from generation_endpoints import router as generation_router
 from user_data_endpoints import router as user_data_router
 from database import create_db_and_tables
 
-app = FastAPI()
+app = FastAPI(docs_url='/docs', redoc_url='/redoc')
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=["http://127.0.0.1:8001", "http://127.0.0.1:8000", "http://localhost:8000", 'https://vibecoder-hex-brainrotai-6cd6.twc1.net'],
